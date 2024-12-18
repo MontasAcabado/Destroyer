3654482414a3aab220a4f099c01709031cb5e0ad
Climbby <franciscoaf.gaming@gmail.com>
2024-12-18 16:29:33 +0000

    Mudancas do francis para testar1

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 pip install py-cord
-pip install audioop-lts 
 pip install python-dotenv
--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    await member.send(
-        f'Welcome to the server, {member.mention}! Enjoy your stay here.'
-    )
+    await member.send(f"Welcome to the server, {member.mention}! Enjoy your stay here.")
 
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
