2f974153a5f850496322c14bbc4734efa3604f20
Climbby <107854416+Climbby@users.noreply.github.com>
2024-12-18 13:12:57 +0000

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,10 +11,17 @@ token = str(os.getenv("TOKEN"))
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+@bot.event
+async def on_member_join(member):
+    await member.send(
+        f'Welcome to the server, {member.mention}! Enjoy your stay here.'
+    )
+
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond(f"Hey! {ctx.user}")
    
+
 
 
 
