ccd6e08f62d5da374eb8bc20994919e7952545b8
Climbby <franciscoaf.gaming@gmail.com>
2024-12-18 17:18:24 +0000

    comentarios

--- main/main.py
+++ main/main.py
@@ -1,11 +1,12 @@
-import discord
 from dotenv import load_dotenv
+import discord
 import os
-
-bot = discord.Bot()
 
 load_dotenv()
 token = str(os.getenv("TOKEN"))
+bot = discord.Bot()
+
+# -------- EVENTOS --------
 
 @bot.event
 async def on_ready():
@@ -15,27 +16,12 @@ async def on_ready():
 async def on_member_join(member):
     await member.send(f"Welcome to the server, {member.mention}! Enjoy your stay here.")
 
+# -------- COMANDOS --------
 
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond(f"Hey! {ctx.user}")
    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 bot.run(os.getenv('TOKEN'))
