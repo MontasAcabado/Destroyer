56822416bdd94d0dec1bd6c039896a0202ed92d5
Climbby <franciscoaf.gaming@gmail.com>
2024-12-20 15:48:40 +0000

    tirei f string

--- comandos/projeto.py
+++ comandos/projeto.py
@@ -4,4 +4,4 @@ def setup(bot):
 
     @bot.slash_command(name="projeto", description="Vai buscar o projeto à net")
     async def projeto(ctx: discord.ApplicationContext):
-        await ctx.respond(f"Link do projeto [Projeto](https://github.com/MontasAcabado/Destroyer)")
+        await ctx.respond("Link do projeto [Projeto](https://github.com/MontasAcabado/Destroyer)")
