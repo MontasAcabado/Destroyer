ee5cf2f07e8bec0dfd584fe036020002ed69e571
Climbby <franciscoaf.gaming@gmail.com>
2024-12-20 23:54:20 +0000

    contador arranjado

--- comandos/contador.py
+++ comandos/contador.py
@@ -2,9 +2,9 @@ import discord
 
 def setup(bot):
 
-    contador = 1
+    contador = {"valor": 1}
 
     @bot.slash_command(name="contar", description="Comeca a contar")
     async def contar(ctx: discord.ApplicationContext):
-        await ctx.respond(f"Contador: {contador}")
-        await contador += 1
+        await ctx.respond(f"Contador: {contador["valor"]}")
+        contador["valor"] += 1    
