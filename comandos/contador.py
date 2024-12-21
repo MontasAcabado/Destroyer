1dc0e425e28047130cb7058881fc0321bf44d86e
Climbby <franciscoaf.gaming@gmail.com>
2024-12-21 11:51:26 +0000

    Adicionei butao ao contador

--- comandos/contador.py
+++ comandos/contador.py
@@ -4,7 +4,12 @@ def setup(bot):
 
     contador = {"valor": 1}
 
+    class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+        @discord.ui.button(label="Clica-me!", style=discord.ButtonStyle.primary) 
+        async def button_callback(self, button, interaction):
+            await interaction.response.send_message("Clicaste no butão!")
+            contador["valor"] += 1 
+
     @bot.slash_command(name="contar", description="Comeca a contar")
     async def contar(ctx: discord.ApplicationContext):
-        await ctx.respond(f"Contador: {contador["valor"]}")
-        contador["valor"] += 1    
+        await ctx.respond(f"Contador: {contador["valor"]}", view=MyView())   
