404ca849c6cbc80e9a82b191eda86374c5c36cf0
Climbby <franciscoaf.gaming@gmail.com>
2024-12-21 19:33:44 +0000

    O Contador agora atualiza quando clicas no botão, o timeout default é 180, o botão desaparece após o timeout

--- comandos/contador.py
+++ comandos/contador.py
@@ -2,14 +2,23 @@ import discord
 
 def setup(bot):
 
-    contador = {"valor": 1}
+    contador = {"valor": 1,
+                "mensagem": None}
 
-    class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    class Botao1(discord.ui.View):
+
         @discord.ui.button(label="Clica-me!", style=discord.ButtonStyle.primary) 
-        async def button_callback(self, button, interaction):
-            await interaction.response.send_message("Clicaste no butão!")
-            contador["valor"] += 1 
+        async def clicar_botao(self, button, interaction):
+            contador["valor"] += 1
+            await interaction.response.defer() 
+            await contador["mensagem"].edit(f"Contador: {contador["valor"]}")
+
+        async def on_timeout(self):
+            self.clear_items()
+            await contador["mensagem"].edit(view=self)
+
 
     @bot.slash_command(name="contar", description="Comeca a contar")
     async def contar(ctx: discord.ApplicationContext):
-        await ctx.respond(f"Contador: {contador["valor"]}", view=MyView())   
+        await ctx.respond(f"Contador: {contador["valor"]}", view=Botao1())
+        contador["mensagem"] = await ctx.interaction.original_response()
