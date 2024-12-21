fff94478280366f3bf9ce9702a5988332348ac9c
Climbby <franciscoaf.gaming@gmail.com>
2024-12-21 19:37:40 +0000

    mesagem agora só aparece para ti

--- comandos/projeto.py
+++ comandos/projeto.py
@@ -4,4 +4,4 @@ def setup(bot):
 
     @bot.slash_command(name="projeto", description="Vai buscar o projeto à net")
     async def projeto(ctx: discord.ApplicationContext):
-        await ctx.respond("Link do [Projeto](https://github.com/MontasAcabado/Destroyer)")
+        await ctx.respond("Link do [Projeto](https://github.com/MontasAcabado/Destroyer)", ephemeral=True)
